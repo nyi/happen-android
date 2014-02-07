3d29111e2541e937c6f30a08edf47730c8e17c74
Nelson <nelsonyi@usc.edu>
2014-02-06 21:08:17 -0800

    WE GOT LISTSSSSS. TIME TO EAT DINNER

--- app/src/main/java/com/happen/app/MainActivity.java
+++ app/src/main/java/com/happen/app/MainActivity.java
@@ -7,6 +7,7 @@ import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.app.ListFragment;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
@@ -16,6 +17,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import com.parse.Parse;
 import com.parse.ParseException;
@@ -39,6 +42,8 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
      * The {@link ViewPager} that will host the section contents.
      */
     ViewPager mViewPager;
+
+    FeedFragment feedFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -132,7 +137,7 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            return FeedFragment.newInstance(position + 1);
         }
 
         @Override
@@ -191,6 +196,43 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
             TextView textView = (TextView) rootView.findViewById(R.id.section_label);
             textView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
             return rootView;
+        }
+    }
+
+    public static class FeedFragment extends ListFragment {
+        String[] numbers_text = new String[] { "one", "two", "three", "four",
+                "five", "six", "seven", "eight", "nine", "ten", "eleven",
+                "twelve", "thirteen", "fourteen", "fifteen" };
+        String[] numbers_digits = new String[] { "1", "2", "3", "4", "5", "6", "7",
+                "8", "9", "10", "11", "12", "13", "14", "15" };
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+
+        public static FeedFragment newInstance(int sectionNumber) {
+            FeedFragment fragment = new FeedFragment();
+            Bundle args = new Bundle();
+            /*args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);*/
+            return fragment;
+        }
+
+        public FeedFragment() {
+
+        }
+
+        @Override
+        public void onListItemClick(ListView l, View v, int position, long id) {
+            //do something
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                    inflater.getContext(), android.R.layout.simple_list_item_1,
+                    numbers_text);
+            setListAdapter(adapter);
+            return super.onCreateView(inflater, container, savedInstanceState);
         }
     }
 
