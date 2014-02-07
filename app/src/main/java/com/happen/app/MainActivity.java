bc789678d9d31f7d38031bfbe45c6b175f15269e
Kevin Sui <ksui92@gmail.com>
2014-02-06 20:28:40 -0800

    changed app name in android and sign in button

--- app/src/main/java/com/happen/app/MainActivity.java
+++ app/src/main/java/com/happen/app/MainActivity.java
@@ -83,29 +83,6 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
         }
     }
 
-    public void createUser(String username, String password, String email, String phone) {
-        ParseUser user = new ParseUser();
-        user.setUsername(username);
-        user.setPassword(password);
-        user.setEmail(email);
-
-        // other fields can be set just like with ParseObject
-        user.put("phone", phone);
-
-        user.signUpInBackground(new SignUpCallback() {
-            public void done(ParseException e) {
-                if (e == null) {
-                    // Hooray! Let them use the app now.
-                } else {
-                    // Sign up didn't succeed. Look at the ParseException
-                    // to figure out what went wrong
-                }
-            }
-        });
-
-    }
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         
