a2d9b80147d7dc45a69df4f73223714cde517af5
Spencer McClure <spencer.mcclure@comcast.net>
2014-04-12 15:59:13 -0700

    news now photogenic

--- app/src/main/java/com/happen/app/components/NewsAdapter.java
+++ app/src/main/java/com/happen/app/components/NewsAdapter.java
@@ -1,10 +1,12 @@
 package com.happen.app.components;
 
+import android.graphics.Bitmap;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.happen.app.R;
@@ -23,11 +25,18 @@ public class NewsAdapter extends BaseAdapter{
     static final String KEY_EMPTY = "empty";
 
     private ArrayList<MainActivity.NewsObject> data;
+    private ArrayList<Bitmap> pictures;
     private static LayoutInflater inflater = null;
     private FriendsFragment parent;
 
     public NewsAdapter(ArrayList<MainActivity.NewsObject> d, LayoutInflater i) {
         data = d;
+        inflater = i;
+    }
+
+    public NewsAdapter(ArrayList<MainActivity.NewsObject> d, ArrayList<Bitmap> pics, LayoutInflater i) {
+        data = d;
+        pictures=pics;
         inflater = i;
     }
 
@@ -68,24 +77,31 @@ public class NewsAdapter extends BaseAdapter{
             String meTooText = " has Me-Too'ed your event.";
             String acceptText = " has accepted your friend request.";
             String requestText = " has sent you a friend request.";
+            vi = inflater.inflate(R.layout.row_news, null);
+            TextView txt = (TextView) vi.findViewById(R.id.news_item_txt);
+            ImageView profilePic = (ImageView)vi.findViewById(R.id.profile_pic);
+
 
             news = data.get(i);
             if(news.type.equals(Util.NEWS_TYPE_ME_TOO)) {
-                vi = inflater.inflate(R.layout.row_news_me_too, null);
-                TextView txt = (TextView) vi.findViewById(R.id.news_item_txt);
                 txt.setText(news.nameSource + meTooText);
             } else if(news.type.equals(Util.NEWS_TYPE_REQ_ACCEPT)){
-                vi = inflater.inflate(R.layout.row_news_request_accepted, null);
-                TextView txt = (TextView) vi.findViewById(R.id.news_item_txt);
                 txt.setText(news.nameSource + acceptText);
             } else if(news.type.equals(Util.NEWS_TYPE_REQ_RECEIVED)){
-                vi = inflater.inflate(R.layout.row_news_request_received, null);
-                TextView txt = (TextView) vi.findViewById(R.id.news_item_txt);
                 txt.setText(news.nameSource + requestText);
+            }
+            if(pictures.size() > i) {
+                profilePic.setImageBitmap(pictures.get(i));
             }
         }
 
         return vi;
+    }
+
+    public void replace(ArrayList<MainActivity.NewsObject> d, ArrayList<Bitmap> p) {
+        this.data = d;
+        this.pictures = p;
+        this.notifyDataSetChanged();
     }
 
     public void replace(ArrayList<MainActivity.NewsObject> d) {
